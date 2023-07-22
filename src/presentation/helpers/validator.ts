b0840cc5cb3f9933984b5b729a2a02a997bf6a30
yousseffel-masry <youssefmasry04@gmail.com>
2023-07-22 07:37:28 +0300

    finalize all

--- src/presentation/helpers/validator.ts
+++ src/presentation/helpers/validator.ts
@@ -1,19 +1,16 @@
-/// regular expression validate email
-const emailRegex = new RegExp(/^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+$/);
-/// regular expression validate password should be 8 characters long and contain at least 1 letter and 1 number
-const passwordRegex = new RegExp(/^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/);
+/// regular expression validate email only by @ and .
+const emailRegex = new RegExp(/^[^\s@]+@[^\s@]+\.[^\s@]+$/);
+
 
 export class Validator {
     static validateEmail = (email: string): boolean => {
         return emailRegex.test(email);
     }
     static validatePassword = (password: string): boolean => {
-        return passwordRegex.test(password);
+        console.log(password);
+        return password.length >= 8;
     }
     static validatePrice = (price: number): boolean => {
         return price > 0;
-    }
-    static validateId = (id: string): boolean => {
-        return id.length>0;
     }
 }
